221a5eae7118374e9672fcb0cef5b06c7550ee24
wikty <wiktymouse@gmail.com>
2014-08-13 08:17:22 +0800

    change PRODUCTION port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from tornado.options import define
@@ -10,8 +11,13 @@ if sys.getdefaultencoding() != SYS_ENCODING:
     reload(sys)  # after reload sys, sys has attribute setdefaultencoding
     sys.setdefaultencoding(SYS_ENCODING)
     
+
+
 # define tornado server listen port
-define("port", default=5000, type=int)
+if 'PRODUCTION' in os.environ:
+    define('port', default=os.environ['PORT'], type=int)
+else:
+    define("port", default=5000, type=int)
 
 if __name__ == '__main__':
 	run()
